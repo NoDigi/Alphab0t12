41bfdf1bca9f237f75b6d1cbb08af16a52b6a273
Oғғɪᴄɪᴀʟ Bᴏᴛ Lʏɴ乂ᴢʏ   ? <98347766+NoDigi@users.noreply.github.com>
2022-02-18 18:58:28 +0800

    Update donasi.js

--- shop/donasi.js
+++ shop/donasi.js
@@ -1,27 +1,11 @@
 const donasibot = () => {
-return `DONASI BISA VIA BANK / E-MONEY
+return `DONASI BISA VIA TOPUP/PIN
 
-*🏧 BANK*
+*🥀UMOBILE*
+• 60189830350
 
-1. BRI 
-	ㅁ 8881 0887 4350 47326
-2. BCA 
-	ㅁ 3901 0887 4350 47326
-3. BNI 
-	ㅁ 881 0887 4350 47326
-4. PERMATA
-	ㅁ 852 8887 4350 47326
-	
-*💰 E-MONEY*
-
-1. GOPAY
-	ㅁ 0838 2275 9168
-2. OVO
-	ㅁ 0838 2275 9168
-3. DANA
-	ㅁ 0838 2275 9168
-	
-Semuanya atas nama Andrii Street Moonshine
+*🥀DIGI*
+• 60149431385
 `
 	}
 
